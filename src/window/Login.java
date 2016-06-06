2eee3400e67489e1c35677cbb7ffe8bfb4c39a5c
poluner <poluner@qq.com>
2016-06-06 15:17:35 +0800

    Update Login.java

--- src/window/Login.java
+++ src/window/Login.java
@@ -6,6 +6,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import client.Client;
+
 import javax.swing.JRadioButton;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
@@ -13,6 +16,7 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import java.awt.event.ActionListener;
+import java.net.Socket;
 import java.awt.event.ActionEvent;
 
 public class Login extends JFrame {
@@ -28,6 +32,7 @@ public class Login extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+
 					new Sql();
 					Login frame = new Login();
 					frame.setVisible(true);
@@ -42,63 +47,67 @@ public class Login extends JFrame {
 	 * Create the frame.
 	 */
 	public Login() {
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
+
 		JPanel panel = new JPanel();
 		contentPane.add(panel, BorderLayout.SOUTH);
-		
+
 		JRadioButton radioButton_teacher = new JRadioButton("教师");
 		panel.add(radioButton_teacher);
-		
+
 		JRadioButton radioButton_student = new JRadioButton("学生");
 		panel.add(radioButton_student);
-		
-		ButtonGroup buttonGroup=new ButtonGroup();
+
+		ButtonGroup buttonGroup = new ButtonGroup();
 		buttonGroup.add(radioButton_teacher);
 		buttonGroup.add(radioButton_student);
-		
-		
+
 		JButton button = new JButton("确定");
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				boolean isTeacher;
-				if(radioButton_teacher.isSelected())isTeacher=true;
-				else if(radioButton_student.isSelected())isTeacher=false;
-				else return;
-				String id=textField.getText();
-				String pw=new String(passwordField.getPassword());
-				if(Sql.pass(isTeacher,id,pw)==false)return;
+				if (radioButton_teacher.isSelected())
+					isTeacher = true;
+				else if (radioButton_student.isSelected())
+					isTeacher = false;
+				else
+					return;
+				String id = textField.getText();
+				String pw = new String(passwordField.getPassword());
+				if (Sql.pass(isTeacher, id, pw) == false)
+					return;
 				new Person(isTeacher, Integer.parseInt(id));
 				dispose();
 			}
 		});
 		panel.add(button);
-		
+
 		JPanel panel_1 = new JPanel();
 		contentPane.add(panel_1, BorderLayout.CENTER);
 		panel_1.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("工号/学号：");
 		lblNewLabel.setBounds(90, 70, 72, 15);
 		panel_1.add(lblNewLabel);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("密码：");
 		lblNewLabel_1.setBounds(90, 114, 54, 15);
 		panel_1.add(lblNewLabel_1);
-		
+
 		textField = new JTextField();
 		textField.setBounds(172, 67, 127, 21);
 		panel_1.add(textField);
 		textField.setColumns(10);
-		
+
 		passwordField = new JPasswordField();
 		passwordField.setBounds(172, 107, 127, 22);
 		panel_1.add(passwordField);
-		
+
 	}
 }
