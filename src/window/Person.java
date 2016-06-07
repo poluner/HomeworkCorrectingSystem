dd3af2943964d70347a548c43cbc45cdc2558044
poluner <poluner@qq.com>
2016-06-07 10:57:44 +0800

    Update Person.java

--- src/window/Person.java
+++ src/window/Person.java
@@ -259,7 +259,7 @@ public class Person extends JFrame implements MouseListener {
 		}
 		else if(e.getSource()==menuItems[2]){//刷新
 			refreshQuestionTable();
-			refreshQuestionTable();
+			refreshAnswerTable();
 		}
 
 		if (e.getSource() == table_course) {
